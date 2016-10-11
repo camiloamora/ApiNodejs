4c825fc0abf25702aeb2e96dc822fdaed37a73ea
CamiloMora <kalufau@hotmail.com>
2016-10-10 21:59:18 -0500

    Metodos get y getId

--- index.js
+++ index.js
@@ -11,15 +11,27 @@ app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 
 app.get('/api/product', (req, res) => {
-    res.send(200, { products: [] })
+    Product.find({}, (err, products) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la petición: ${err}` })
+        if (!products) return res.status(404).send({message: 'No existen productos' })
+    
+    res.send(200, { products })
+    })
 })
 
-app.get('/api/productid/:productId', (req, res) => {
+app.get('/api/product/:productId', (req, res) => {
+    let productId = req.params.productId
 
+    Product.findById(productId, (err, product) => {
+        if(err) return res.status(500).send({ message: `Error al realizar la petición: ${err}` })
+        if(!product) return res.status(404).send({ message: 'El producto no existe'})
+
+        res.status(200).send({ product })
+    })
 })
 
 app.post('/api/product', (req, res) => {
-    console.log('POST/api/product')
+    console.log('POST/api/product') 
     console.log(req.body)
 
     let product = new Product()
@@ -41,7 +53,7 @@ app.put('api/product/:productId', (req, res) => {
 })
 
 app.delete('api/product/:productId', (req, res) => {
-
+    
 })
 
 mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
